d943e1b3e728588927f53d405f36541af2669c3b
Andrew Bancroft <acbancroft@NEWELLMA-MAC.local>
2015-07-14 12:34:37 -0500

    Set up table view with data.

--- SwipeToDeleteExample/ViewController.swift
+++ SwipeToDeleteExample/ViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    let planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Team Pluto!"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return planets.count
     }
-
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BasicCell") as! UITableViewCell
+        cell.textLabel?.text = planets[indexPath.row]
+        
+        return cell
+    }
 }
 
