4d49a513b43fb59def88ae287189a1bf9540be82
Andrew Bancroft <acbancroft@NEWELLMA-MAC.local>
2015-07-15 12:55:28 -0500

    Implemented delete handling

--- SwipeToDeleteExample/ViewController.swift
+++ SwipeToDeleteExample/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     
-    let planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Team Pluto!"]
+    var planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Team Pluto!"]
     var deletePlanetIndexPath: NSIndexPath? = nil
     
     override func viewDidLoad() {
@@ -35,25 +35,42 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // MARK: UITableViewDelegate Methods
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
+            deletePlanetIndexPath = indexPath
             let planetToDelete = planets[indexPath.row]
             confirmDelete(planetToDelete)
         }
     }
     
+    // Delete Confirmation and Handling
     func confirmDelete(planet: String) {
         let alert = UIAlertController(title: "Delete Planet", message: "Are you sure you want to permanently delete \(planet)?", preferredStyle: .ActionSheet)
-        let CancelAction = UIAlertAction(title: "Cancel", style: .Default, handler: nil)
+        
         let DeleteAction = UIAlertAction(title: "Delete", style: .Destructive, handler: handleDeletePlanet)
-        alert.addAction(CancelAction)
+        let CancelAction = UIAlertAction(title: "Cancel", style: .Default, handler: cancelDeletePlanet)
+        
         alert.addAction(DeleteAction)
+        alert.addAction(CancelAction)
         
         self.presentViewController(alert, animated: true, completion: nil)
     }
     
     func handleDeletePlanet(alertAction: UIAlertAction!) -> Void {
         if let indexPath = deletePlanetIndexPath {
+            tableView.beginUpdates()
             
+            planets.removeAtIndex(indexPath.row)
+            
+            // Note that indexPath is wrapped in an array:  [indexPath]
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+            
+            deletePlanetIndexPath = nil
+            
+            tableView.endUpdates()
         }
+    }
+    
+    func cancelDeletePlanet(alertAction: UIAlertAction!) {
+        deletePlanetIndexPath = nil
     }
 }
 
